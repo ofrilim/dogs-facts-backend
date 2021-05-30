c8a23bef1d31ac5b70407e7e0c4849415d1951af
ofrilim <ofrilim@gmail.com>
2021-05-30 08:55:16 +0300

    uploading project

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());  // parse the req.body jsons responses
+app.use(cookieParser());  // parse the cookies
+app.use(cors({credentials: true, origin: ['http://localhost:3000']}));
+
+
+const addRoutes = require('./routes/users.route');
+addRoutes(app);
+
+app.listen(PORT, () => {
+  console.log('server is listening')
+});
